caa12ca0d3a08818f5d1f60b525f3fe4085af31c
toxinoid00 <senzpmd@gmail.com>
2017-01-13 05:23:29 +0700

    Revert "Delete contact.component.ts"
    This reverts commit d1cc42614ab319efa701fea0a0cf366c527a5c4e.

---
+++ app/contact.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+import {FORM_DIRECTIVES} from "angular2/common";
+
+@Component({
+    selector:'contact',
+    directives:[FORM_DIRECTIVES],
+    templateUrl: "../app/views/contact.html",
+})
+
+export class Contact {
+
+}
